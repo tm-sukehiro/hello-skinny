3959f1fa5cd5be9c6789271dbf7fe8ed6db32bb9
tm-sukehiro <k.sukehiro@tcmobile.jp>
2016-06-17 16:50:52 +0900

    XMLをPOSTして認証するAPIパターンの雛形を作成

--- src/main/scala/controller/XMLController.scala
+++ src/main/scala/controller/XMLController.scala
@@ -7,13 +7,24 @@ import scala.xml.{NodeSeq, XML}
 
 class XMLController extends ApplicationController {
   def index = {
-    val response: Response = HTTP.get("http://d.hatena.ne.jp/changes.xml")
+    // リクエスト用のXML
+    val request = <request>
+      <user_id>USER_ID</user_id>
+      <password>PASSWORD</password>
+    </request>
 
+    // 認証用URLにXMLをPOSTする
+    val auth: Response = HTTP.request(Method.POST, Request("http://localhost:8080/auth").body(request.text.getBytes).contentType("application/xml"))
+
+    // Set-Cookieにマルチバリューで認証情報が入っているので;で連結する
+    val cookies = auth.headerField("Set-Cookie").map(_.split(";")(0)).mkString(";")
+    // Cookieに認証情報を入れてAPIをGETする
+    val response: Response = HTTP.request(Method.GET, Request("http://localhost:8080/product").header("Cookie", cookies))
+
+    // レスポンスをスクリプティングする
     val xml = XML.loadString(response.textBody)
-    val a: NodeSeq = xml \\ "@name"
+    val name: NodeSeq = xml \\ "@name"
 
-    response.headers.foreach(t => println(t))
-
-    Ok(response.textBody, contentType = Some("application/xml"))
+    Ok(name.text, contentType = Some("application/xml"))
   }
 }
